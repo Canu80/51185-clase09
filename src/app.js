318225f1459084074940011d2ba95717113a6beb
Canu80 <114115064+Canu80@users.noreply.github.com>
2023-04-12 21:21:32 -0400

    Primer pre entrega

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cartsRouter from "./routes/carts.router.js";
+import productsRouter from "./routes/products.router.js";
+
+const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+app.listen(PORT, ()=>{
+    console.log('Servidor funcionando en el puerto: ' + PORT)
+})
+
+app.use('/api/carts', cartsRouter);
+app.use('/api/products', productsRouter);
